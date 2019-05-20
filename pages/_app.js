afcf22ba73fbe6aac28f1b7105ebf10278388c1d
gonzalovazquez <gonzalovazquez010@gmail.com>
2019-05-20 12:09:45 -0400

    style: 💄 add favicon support

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,13 @@ export default class ProjectViewerApp extends App {
     return (
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+        <link rel="manifest" href="/static/site.webmanifest" />
+        <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#ffffff" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
