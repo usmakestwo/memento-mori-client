ca9eb6a959217ef2ee452880182e67f72da0528b
gonzalovazquez <gonzalovazquez010@gmail.com>
2019-06-07 16:46:52 -0400

    feat: 🎸 replace express with fastify

--- server.js
+++ server.js
@@ -1,52 +1,46 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-shadow */
 /* eslint-disable no-console */
-const express = require('express')
-const next = require('next')
-const { join, resolve } = require('path')
-const { parse } = require('url')
-const compression = require('compression')
+const fastify = require('fastify')({ logger: { level: 'error' } })
+const Next = require('next')
+
 const { version } = require('./package.json')
 
+const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dir: '.', dev })
 
-const handle = app.getRequestHandler()
-const port = process.env.PORT ? process.env.PORT : 3000
+fastify.register((fastify, opts, next) => {
+  const app = Next({ dev })
+  app
+    .prepare()
+    .then(() => {
+      if (dev) {
+        fastify.get('/_next/*', async (req, reply) => {
+          await app.handleRequest(req.req, reply.res)
+          reply.sent = true
+        })
+      }
 
-const ServiceWorker = _app => (req, res) => {
-  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
-  res.set('Content-Type', 'application/javascript')
-  const parsedUrl = parse(req.url, true)
-  const { pathname } = parsedUrl
-  const filePath = join(__dirname, '.next', pathname)
-  if (dev) {
-    console.log('dev - sw from static')
-    _app.serveStatic(req, res, resolve('./static/service-worker.js'))
-  } else {
-    _app.serveStatic(req, res, filePath)
-  }
-}
+      fastify.get('/version', (req, reply) => {
+        reply.send({ version })
+      })
 
-app
-  .prepare()
-  .then(() => {
-    const server = express()
-    server.use(compression())
+      fastify.get('/*', async (req, reply) => {
+        await app.handleRequest(req.req, reply.res)
+        reply.sent = true
+      })
 
-    server.get('/service-worker.js', ServiceWorker(app))
-    // readiness & liveness probe endpoint
-    server.get('/version', (req, res) => {
-      res.json({ version })
+      fastify.setNotFoundHandler(async (request, reply) => {
+        await app.render404(request.req, reply.res)
+        reply.sent = true
+      })
+
+      next()
     })
+    .catch(err => next(err))
+})
 
-    // Route all other urls to next "as is" - see note above about /next/ and /webpack/
-    server.get('*', (req, res) => handle(req, res))
-
-    server.listen(port, (err) => {
-      if (err) throw err
-      console.log(`> Ready on http://localhost:${port}`)
-    })
-  })
-  .catch((ex) => {
-    console.error(ex.stack)
-    process.exit(1)
-  })
+fastify.listen(port, (err) => {
+  if (err) throw err
+  console.log(`> Ready on http://localhost:${port}`)
+})
